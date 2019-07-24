69b764d1e1dc518f235a66d401d6fa8e830f76a6
YeJ XU <nhxyj111@gmail.com>
2019-07-24 08:21:36 +0800

    OneToOne

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 import { User } from './entity/User'
 import { Photo } from './entity/Photo'
+import { PhotoMetadata } from './entity/PhotoMetadata'
 
 createConnection()
   .then(async connection => {
@@ -11,22 +12,45 @@ createConnection()
     user.lastName = 'Saw'
     user.age = 25
     await connection.manager.save(user)
-    console.log('Saved a new user with id: ' + user.id)
+    // console.log('Saved a new user with id: ' + user.id)
 
-    console.log('Loading users from the database...')
+    // console.log('Loading users from the database...')
     const users = await connection.manager.find(User)
-    console.log('Loaded users: ', users)
+    // console.log('Loaded users: ', users)
 
-    console.log('Here you can setup and run express/koa/any other framework.')
+    // console.log('Here you can setup and run express/koa/any other framework.')
 
     let photo = new Photo()
-    photo.name = 'Me and Bears'
+    photo.name = 'Me and Bears 2-2 '
     photo.description = 'I am near polar bears'
     photo.filename = 'photo-with-bears.jpg'
     photo.views = 1
     photo.isPublished = true
-    await connection.manager.save(photo)
-    let savedPhotos = await connection.manager.find(Photo)
-    console.log('All photos from the db: ', savedPhotos)
+
+    // create a photo metadata
+    let metadata = new PhotoMetadata()
+    metadata.height = 640
+    metadata.width = 480
+    metadata.compressed = true
+    metadata.comment = 'cybershoot'
+    metadata.orientation = 'portait'
+    metadata.photo = photo // this way we connect them
+
+    let photoRepository = connection.getRepository(Photo)
+    let metadataRepository = connection.getRepository(PhotoMetadata)
+
+    await photoRepository.save(photo)
+    await metadataRepository.save(metadata)
+
+    let photos = await photoRepository.find({ relations: ['metadata'] })
+    console.log(photos)
+    // console.log('Photo has been saved')
+
+    // let savedPhotos = await photoRepository.find()
+    // console.log('All photos from the db: ', savedPhotos)
+
+    // let [allPhotos, photosCount] = await photoRepository.findAndCount()
+    // console.log('All photos: ', allPhotos)
+    // console.log('Photos count: ', photosCount)
   })
   .catch(error => console.log(error))
