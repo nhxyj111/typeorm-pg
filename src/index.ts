479c77fdea9f6153d3b4f6fb3cf28de30251a6fa
YeJ XU <nhxyj111@gmail.com>
2019-07-24 12:22:13 +0800

    o

--- src/index.ts
+++ src/index.ts
@@ -26,5 +26,18 @@ createConnection()
     await photoRepository.save(photo)
 
     console.log(await photoRepository.findOne(1, { relations: ['albums'] }))
+
+    let photos = await connection
+      .getRepository(Photo)
+      .createQueryBuilder('photo') // first argument is an alias. Alias is what you are selecting - photos. You must specify it.
+      .innerJoinAndSelect('photo.metadata', 'metadata')
+      .leftJoinAndSelect('photo.albums', 'album')
+      .where('photo.isPublished = true')
+      .andWhere('(photo.name = :photoName OR photo.name = :bearName)')
+      .orderBy('photo.id', 'DESC')
+      .skip(5)
+      .take(10)
+      .setParameters({ photoName: 'My', bearName: 'Mishka' })
+      .getMany()
   })
   .catch(error => console.log(error))
